c255ba1f8e45393a1428401f4c10eaf9856bb0c4
Thomas Burtin <tburtin@paul-f4Ar7s9.clusters.42paris.fr>
2025-01-06 13:05:38 +0100

    fresg

---
+++ ex02/table.py
@@ -0,0 +1,38 @@
+import psycopg2
+import pandas
+import os
+
+def main():
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="piscineds",
+            user="tburtin",
+            password="toto"
+        )
+        cur = conn.cursor()
+
+        file = "data_2022_dec.csv"
+        file_name_without_extension = os.path.splitext(file)[0]
+
+        cur.execute(f"""
+        CREATE TABLE {file_name_without_extension} (
+            event_time TIMESTAMP,
+            event_type VARCHAR(50),
+            product_id INT,
+            price FLOAT,
+            user_id INT,
+            user_session INT
+        );
+        """)
+
+        cur.close()
+        conn.commit()
+        conn.close()
+
+    except Exception as error:
+        print(f"{Exception.__name__} : {error}")
+
+
+if __name__ == "__main__":
+    main()
