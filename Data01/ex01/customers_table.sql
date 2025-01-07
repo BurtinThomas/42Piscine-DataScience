fe0bb9e11469f682b752b59fc70eea00d618d977
Francois <transfo@Francoiss-MacBook-Air.local>
2025-01-07 21:02:53 +0100

    v3

--- Data01/ex01/customers_table.sql
+++ Data01/ex01/customers_table.sql
@@ -1,10 +1,22 @@
-CREATE TABLE IF NOT EXISTS customers AS
+CREATE TABLE IF NOT EXISTS customers (
+    event_time TIMESTAMP,
+    event_type VARCHAR(255),
+    product_id SERIAL,
+    price DECIMAL,
+    user_id SERIAL,
+    user_session VARCHAR(36)
+);
+
+COPY customers (event_time, event_type, product_id, price, user_id, user_session)
+FROM '../subject/customer/data_2023_feb.csv'
+DELIMITER ','
+CSV HEADER;
+
+INSERT INTO customers
 SELECT * FROM data_2022_dec
-UNION ALL
+UNION
 SELECT * FROM data_2022_nov
-UNION ALL
+UNION
 SELECT * FROM data_2022_oct
-UNION ALL
-SELECT * FROM data_2023_jan
-UNION ALL
-SELECT * FROM data_2023_feb;
+UNION
+SELECT * FROM data_2023_jan;
